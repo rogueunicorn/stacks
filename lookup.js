d86797e50532388d447048f8f2f8574e7c36b565
Toby Hede <tobyhede@info-architects.net>
2016-01-20 08:25:11 +1100

    Use AWS Lambda for cross-stack lookup

---
+++ lookup.js
@@ -0,0 +1,85 @@
+/* Copyright 2015 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+   This file is licensed to you under the AWS Customer Agreement (the "License").
+   You may not use this file except in compliance with the License.
+   A copy of the License is located at http://aws.amazon.com/agreement/.
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+   See the License for the specific language governing permissions and limitations under the License. */
+var response = {
+  SUCCESS: "SUCCESS",
+  FAILED: "FAILED",
+
+  send: function(event, context, responseStatus, responseData, physicalResourceId) {
+
+    var responseBody = JSON.stringify({
+        Status: responseStatus,
+        Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
+        PhysicalResourceId: physicalResourceId || context.logStreamName,
+        StackId: event.StackId,
+        RequestId: event.RequestId,
+        LogicalResourceId: event.LogicalResourceId,
+        Data: responseData
+    });
+
+    console.log("Response body:\n", responseBody);
+
+    var https = require("https");
+    var url = require("url");
+
+    var parsedUrl = url.parse(event.ResponseURL);
+    var options = {
+        hostname: parsedUrl.hostname,
+        port: 443,
+        path: parsedUrl.path,
+        method: "PUT",
+        headers: {
+            "content-type": "",
+            "content-length": responseBody.length
+        }
+    };
+
+    var request = https.request(options, function(response) {
+        console.log("Status code: " + response.statusCode);
+        console.log("Status message: " + response.statusMessage);
+        context.done();
+    });
+
+    request.on("error", function(error) {
+        console.log("send(..) failed executing https.request(..): " + error);
+        context.done();
+    });
+
+    request.write(responseBody);
+    request.end();
+  }
+}
+
+exports.handler = function(event, context) {
+    console.log('REQUEST RECEIVED:\\n', JSON.stringify(event));
+    if (event.RequestType == 'Delete') {
+        response.send(event, context, response.SUCCESS);
+        return;
+    }
+    var stackName = event.ResourceProperties.StackName;
+    var responseData = {};
+    if (stackName) {
+        var aws = require('aws-sdk');
+        var cfn = new aws.CloudFormation();
+        cfn.describeStacks({StackName: stackName}, function(err, data) {
+            if (err) {
+                responseData = {Error: 'DescribeStacks call failed'};
+                console.log(responseData.Error + ':\\n', err);
+                response.send(event, context, response.FAILED, responseData);
+            }
+            else {
+                data.Stacks[0].Outputs.forEach(function(output) {
+                    responseData[output.OutputKey] = output.OutputValue;
+                });
+                response.send(event, context, response.SUCCESS, responseData);
+            }
+        });
+    } else {
+        responseData = {Error: 'Stack name not specified'};
+        console.log(responseData.Error);
+        response.send(event, context, response.FAILED, responseData);
+    }
+}
